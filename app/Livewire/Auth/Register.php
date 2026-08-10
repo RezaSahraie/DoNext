c4a0ceec717a8c494f3fbd308786c5a75247b4ef
Reza Sahraie <Sahraireza0@gmail.com>
2026-08-11 01:37:46 +0330

    feat: add auth livewire components
    - Add datetime casts to Task model
    - Add Livewire authentication components
    - Add Livewire authentication views

---
+++ app/Livewire/Auth/Register.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Livewire\Auth;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Livewire\Component;
+
+class Register extends Component
+{
+    public string $name = '';
+    public string $email = '';
+    public string $password = '';
+    public string $password_confirmation = '';
+
+    /**
+     * Register new User
+     */
+
+    public function register() : void {
+        $validated = $this->validate([
+            'name' => ['required', 'string', 'max:255'],
+
+            'email' => ['required', 'unique:users,email', 'string', 'email', 'max:255'],
+
+            'password' => ['required', 'confirmed', 'string', 'min:8'],
+        ]);
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
+        Auth::login($user);
+
+        session()->regenerate();
+        $this->redirectRoute('dashboard');
+    }
+    
+
+    public function render()
+    {
+        return view('livewire.auth.register');
+    }
+}
